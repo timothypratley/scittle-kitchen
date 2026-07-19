2f15237d05e59fc1d3f17d2dab7870688761d124
Timothy Pratley <timothypratley@gmail.com>
2026-07-19 15:34:05 -0700

    clojure not clj

--- src/scittle_kitchen/build.clj
+++ src/scittle_kitchen/build.clj
@@ -327,7 +327,7 @@
             (println "scittle-kitchen build Dry run complete - skipping compilation")
             (do
               (println "scittle-kitchen build Compiling ClojureScript")
-              (shell {:dir build-dir} "clj" "-M" "-m" "shadow.cljs.npm-deps")
+              (shell {:dir build-dir} "clojure" "-M" "-m" "shadow.cljs.npm-deps")
               (shell {:dir build-dir} "bb" "release")
               (println "scittle-kitchen build Copying js to" (str dest))
               (fs/copy-tree (fs/file build-dir "resources" "public" "js")
